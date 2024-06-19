6144e5f1203c5793d99279906e3a973b9f929386
Mayaaa311 <mayaaa@umich.edu>
2024-06-19 13:10:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GPTLens-2.0
+
+This is a updated version of GPTLens, aiming to automating the process and feedbacks between the auditor and critic.
---
+++ src/main.py
@@ -0,0 +1,31 @@
+from detector_pkg.bug_detector import Detector
+
+detector = Detector(
+    model_id="TheBloke/Llama-2-7B-GGUF",
+    model_file="llama-2-7b.Q5_K_M.gguf"
+)
+
+sample_code = """
+pragma solidity ^0.8.0;
+mapping(address => uint256) public balances;
+
+function deposit() public payable {
+    balances[msg.sender] += msg.value;
+}
+
+function withdraw(uint256 amount) public {
+    require(balances[msg.sender] >= amount, "Insufficient balance");
+    payable(msg.sender).transfer(amount);
+    balances[msg.sender] -= amount;
+}
+"""
+
+vulnerabilities = detector.run_pipeline(sample_code)
+for vulnerability in vulnerabilities:
+    print(vulnerability)
+    detector.logger.info(f'Ranked Vulnerability: {json.dumps(vulnerability, indent=2)}')
+
+detector.save_results()  # Save results to default result folder
+
+# Or save to a specific path
+# detector.save_results(path="path/to/your/file.txt")
