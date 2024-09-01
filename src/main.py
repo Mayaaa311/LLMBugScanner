deab83937de1bbef55e3d4abc8415edbf34074a6
mayaaa311 <mayaaa@umich.edu>
2024-09-01 03:57:04 -0400

    new result

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ def main():
                         ranker_template_path='templates/topk.txt')
 
     # Run the pipeline with the sample code
-    detector.run_pipeline(code_path="data/2018-10299.sol", topk="3",  output="2018-10299_codellama-S", result_dir = "result_10299")
+    detector.run_pipeline(code_path="data/2018-10299.sol", topk="10",  output="2018-10299_codellama-S-k10", result_dir = "result_10299")
     # detector.run_pipeline(code_path="data/2018-10299.sol", topk="10",  output="2018-10299_k10_n5_t9", result_dir = "result_10299")
 
 
