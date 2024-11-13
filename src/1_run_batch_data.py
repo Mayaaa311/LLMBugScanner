8a86e8999538d4bdfa28b4e231bd7b1387845e95
mayaaa311 <mayaaa@umich.edu>
2024-11-13 15:41:45 -0500

    eval cleaned up

--- src/1_run_batch_data.py
+++ src/1_run_batch_data.py
@@ -9,13 +9,13 @@ sbatch_template = """#!/bin/bash
 #SBATCH --gpus=2                        # Request 4 GPUs
 #SBATCH --mem=200G                      # Increase memory
 #SBATCH -t 100                          # Duration of the job
-#SBATCH -o Report-%j-allnx.out                # Combined output and error messages file
+#SBATCH -o Report-%j-{i}.out   # Combined output and error messages file
 #SBATCH --mail-type=FAIL      # Mail preferences
 #SBATCH --mail-user=yyuan394@gatech.edu # E-mail address for notifications
 
 cd $SLURM_SUBMIT_DIR                    # Correctly change to the submit directory
 
-module load anaconda3/2023.03           # Load module dependencies
+module load anaconda3/2023.03       
 conda activate /home/hice1/yyuan394/scratch/env
 
 python src/bugscanner_cli.py -a {model_name} -c {model_name} -r {model_name} -p {model_name} -d {data_folder} -o {result_folder} -k {k} -log logger
@@ -24,7 +24,7 @@ python src/bugscanner_cli.py -a {model_name} -c {model_name} -r {model_name} -p 
 
 # ------------------------------------------Change below definition to run and custom result folder name-------------------------------
 # change this to the model you want to test
-model_name = 'NTQAI/Nxcode-CQ-7B-orpo '
+model_name = 'NTQAI/Nxcode-CQ-7B-orpo'
 # change this to the data folder you want to run
 data_path = 'data_full/CVE_clean_organized_b5'
 # changet this to the k you want to run
@@ -47,7 +47,7 @@ os.makedirs(sbatch_output_path, exist_ok=True)
 for i, subfolder in enumerate(os.listdir(data_path)):
     full_subfolder_path = os.path.join(data_path, subfolder)
     if os.path.isdir(full_subfolder_path):
-        sbatch_content = sbatch_template.format(data_folder=full_subfolder_path, result_folder = result_folder_name, model_name = model_name, k = k)
+        sbatch_content = sbatch_template.format(data_folder=full_subfolder_path, result_folder = result_folder_name, model_name = model_name, k = k, i = i)
         sbatch_file_path = os.path.join(sbatch_output_path, f'batch_{i}.sbatch')
         
         # Write the sbatch file
